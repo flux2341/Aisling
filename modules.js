5e040177c93c05e4de53a261bdf97451976bb561
flux2341 <flux2341@gmail.com>
2017-12-09 21:02:03 -0800

    addet split.js, basic new/edit functionality

--- modules.js
+++ modules.js
@@ -7,12 +7,12 @@ class ModuleWord {
         container.appendChild(this.input_word);
     }
     switch_to_view_mode(entry) {
-        this.show_word.innerText = entry.word;
+        this.show_word.innerText = entry? entry.word: '';
         this.show_word.style.display = 'block';
         this.input_word.style.display = 'none';
     }
     switch_to_edit_mode(entry) {
-        this.input_word.value = entry.word;
+        this.input_word.value = entry? entry.word: '';
         this.show_word.style.display = 'none';
         this.input_word.style.display = 'block';
     }
@@ -30,23 +30,25 @@ class ModuleWord {
 
 class ModuleDefinition {
     constructor(container) {
+        container.style.height = '200px';
         this.show_definition = document.createElement('span');
         this.input_definition = document.createElement('textarea');
+        this.input_definition.classList.add('scrollable');
         container.appendChild(this.show_definition);
         container.appendChild(this.input_definition);
     }
     switch_to_view_mode(entry) {
-        this.show_definition.innerText = entry.definition;
+        this.show_definition.innerText = entry? entry.definition: '';
         this.show_definition.style.display = 'block';
         this.input_definition.style.display = 'none';
     }
     switch_to_edit_mode(entry) {
-        this.input_definition.value = entry.definition;
+        this.input_definition.value = entry? entry.definition: '';
         this.show_definition.style.display = 'none';
         this.input_definition.style.display = 'block';
     }
     get_search_prefix() {
-        return 'definition'
+        return 'definition';
     }
     save(entry) {
         entry.definition = this.input_definition.value;

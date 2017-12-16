9d63f8b54835f45be82452cf9cd219fd235f11f6
flux2341 <flux2341@gmail.com>
2017-12-15 21:18:27 -0800

    new/edit/save/delete seems to be working

--- index.js
+++ index.js
@@ -9,11 +9,21 @@ var app = new Vue({
     el: '#app',
     data: {
         entries: [{word:'test1', definition:'t1d'},{word:'test2',definition:'t2d'},{word:'test3',definition:'t3d'}],
-        current_entry: {word:'test1', definition:'test1 definition'},
-        temp_entry:    {word:'test1', definition:'test1 definition'},
+        
+        // a default or 'blank' entry, used:
+        // - when the application starts and there isn't a last_selected_word set
+        // - when a new entry is created
+        // - when an entry is deleted and the detail view needs to be cleared
+        null_entry: {word:'', definition:''},
+
+        // the selected entry
+        current_entry: null,
+
+        // the temp_entry is used for distinguishing between 
+        temp_entry:    null,
         mode: 'view',
         modules: ['module-word', 'module-definition'],
-        search_text: '1'
+        search_text: ''
     },
     components: {
         'module-word': {
@@ -32,20 +42,26 @@ var app = new Vue({
         }
     },
     methods: {
+
         load_from_storage: function() {
             let storage_path = settings.get('storage_path');
-            storage.setDataPath(storage_pathpath);
+            storage.setDataPath(storage_path);
+            let app = this;
             storage.getAll(function(error, entries) {
                 if (error) {
                     console.log(error);
                 } else {
-                    this.entries = entries;
+                    app.entries = [];
+                    for (name in entries) {
+                        app.entries.push(entries[name]);
+                    }
                 }
             });
         },
 
         view_entry: function(entry) {
             if (this.mode === 'edit') {
+                let app = this;
                 show_alert({
                     message: 'cancel edit?',
                     buttons: ['yes', 'no'],
@@ -53,24 +69,24 @@ var app = new Vue({
                         if (response === 'yes') {
                             this.mode = 'view';
                         } else {
-                            this.current_entry = entry;
-                            this.temp_entry = this.copy_entry(entry);
+                            app.current_entry = entry;
                         }
                     }
                 });
             } else {
                 this.current_entry = entry;
-                this.temp_entry = this.copy_entry(entry);
             }
         },
         new_entry: function() {
-            this.temp_entry = {word:'', definition:''};
-            this.current_entry = null;
+            this.current_entry = this.copy_entry(this.null_entry);
             this.mode = 'edit';
         },
         edit_entry: function() {
-            this.temp_entry = this.copy_entry(entry);
             this.mode = 'edit';
+        },
+        cancel_edit: function() {
+            this.temp_entry = this.copy_entry(this.current_entry);
+            this.mode = 'view';
         },
         save_entry: function() {
 
@@ -89,8 +105,9 @@ var app = new Vue({
             if (this.temp_entry.word !== this.current_entry.word) {
 
                 // check if the saved word already exists
-                found_index = this.entries.find(function(entry) {
-                    return entry.word === this.temp_entry.word;
+                let app = this;
+                let found_index = this.entries.find(function(entry) {
+                    return entry.word === app.temp_entry.word;
                 });
                 if (found_index >= 0) {
                     show_alert({
@@ -100,16 +117,22 @@ var app = new Vue({
                     });
                     return;
                 }
-                storage.remove(this.current_entry.word, function(error) {
-                    if (error) {
-                        console.log(error);
-                    }
-                });
+                
+                if (this.current_entry.word !== '') {
+                    // remove the 'old' word
+                    storage.remove(this.current_entry.word, function(error) {
+                        if (error) {
+                            console.log(error);
+                        }
+                    });
+                }
             }
 
-            // replace the current_entry with the temp_entry in entries
+            // add the new entry to the entries
+            // or replace the current_entry with the temp_entry in entries
+            let app = this;
             let index = this.entries.findIndex(function(entry) {
-                return entry === this.current_entry;
+                return entry === app.current_entry;
             });
             if (index >= 0) {
                 this.entries[index] = this.temp_entry;
@@ -117,31 +140,41 @@ var app = new Vue({
                 this.entries.push(this.temp_entry);
             }
             this.current_entry = this.temp_entry;
+
+            // sort the entries to keep them in alphabetical order
             this.entries.sort(function(e0, e1) {
                 return e0.word.localeCompare(e1.word);
             });
+
+            // switch the mode to view
             this.mode = 'view'
-            storage.set(current_entry.word, current_entry, function(error) {
+
+            // save the word
+            storage.set(this.current_entry.word, this.current_entry, function(error) {
                 if (error) {
                     console.log(error);
                 }
             });
         },
-        delete_word: function() {
+        delete_entry: function() {
+            let app = this;
             show_alert({
                 message: 'are you sure?',
                 buttons: ['yes', 'no'],
                 callback: function(response) {
                     if (response === 'yes') {
-                        if (current_entry) {
-                            storage.remove(current_entry.word, function(error) {
-                                if (error) {
-                                    console.log(error);
-                                }
-                            });
-                        } else {
-                            view_entry(null);
-                        }
+                        storage.remove(app.current_entry.word, function(error) {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                let index = app.entries.findIndex(function(entry) {
+                                    return entry === app.current_entry;
+                                });
+                                app.entries.splice(index, 1);
+                                app.current_entry = app.copy_entry(app.null_entry);
+                                app.mode = 'view';
+                            }
+                        });
                     }
                 }
             });
@@ -155,10 +188,9 @@ var app = new Vue({
             if (cind >= 0) {
                 let name = st.substring(0, cind);
                 st = st.substring(cind+1);
-                console.log(name);
-                console.log(st);
             }
-            return this.entries.filter((entry) => {
+            
+            let filtered_entries = this.entries.filter((entry) => {
                 if (cind >= 0) {
                     if (name === 'word') {
                         return entry.word.includes(st);
@@ -168,29 +200,49 @@ var app = new Vue({
                 }
                 return entry.word.includes(st) || entry.definition.includes(st);
             });
+            return filtered_entries;
         }
+    },
+    watch : {
+        current_entry: function(val) {
+            this.temp_entry = this.copy_entry(this.current_entry);
+        }
+    },
+    created: function () {
+
+        //settings.deleteAll();
+        if (!settings.has('storage_path')) {
+            let result = dialog.showOpenDialog({properties: ['openDirectory']});
+            settings.set('storage_path', result[0]);
+        }
+        this.load_from_storage();
+
+        let last_selected_word  = settings.get('last_selected_word', null);
+        this.current_entry = this.entries.find((entry) => {
+            return entry.word === last_selected_word;
+        });
+        if (!this.current_entry) {
+            this.current_entry = this.copy_entry(this.null_entry);
+        }
+        // the watch is only executed after the page loads
+        // so we have to set this manually
+        this.temp_entry = this.copy_entry(this.current_entry);
+
+        this.search_text = settings.get('last_search_text', '');
+
+        var Split = require('split.js');
+        Split(['#div_entries', '#div_detail'], {
+            sizes: [20, 80],
+            gutterSize: 20,
+            minSize: [5, 5],
+            direction: 'horizontal',
+            cursor: 'col-resize',
+            snapOffset: 0
+        });
+
     }
 });
 
-//settings.deleteAll();
-if (!settings.has('storage_path')) {
-    let result = dialog.showOpenDialog({properties: ['openDirectory']});
-    settings.set('storage_path', result[0]);
-}
-
-let storage_path = settings.get('storage_path');
-let last_search_text = settings.get('last_search_text');
-let last_selected_word = settings.get('last_selected_word');
-
-var Split = require('split.js');
-Split(['#div_entries', '#div_detail'], {
-    sizes: [20, 80],
-    gutterSize: 20,
-    minSize: [5, 5],
-    direction: 'horizontal',
-    cursor: 'col-resize',
-    snapOffset: 0
-});
 
 
 
@@ -201,7 +253,9 @@ function show_alert(obj) {
     let div_alert_buttons = document.querySelector('#div_alert_buttons');
 
     div_alert_text.innerText = obj.message;
-    clear_children(div_alert_buttons);
+    while (div_alert_buttons.hasChildNodes()) {
+        div_alert_buttons.removeChild(div_alert_buttons.firstChild);
+    }
     for (let i=0; i<obj.buttons.length; ++i) {
         let div_bt = document.createElement('div');
         div_bt.innerText = obj.buttons[i];
